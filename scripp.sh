b1a650280959554be910a14533d0b15e5428aa24
uiop31753 <uiop31753@gmail.com>
2025-04-23 16:27:11 +0800

    Update scripp.sh

--- scripp.sh
+++ scripp.sh
@@ -55,14 +55,14 @@ if [ "$MODE" == "frps" ]; then
 [common]
 bind_port =5244
 dashboard_port = 5426
-dashboard_user = adminn
-dashboard_pwd = Wxc.123456
+dashboard_user = ''
+dashboard_pwd = ''
 EOF
 else
   sudo tee $INSTALL_DIR/frpc.ini > /dev/null <<EOF
 [common]
-server_addr = 47.101.213.4
-server_port =5700
+server_addr = ''
+server_port =''
 
 [ssh]
 type = tcp
